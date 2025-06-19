1ea9d986257dd7581cb3c98e7d2fc68b7957170d
Sandy <93102462+sandycdutra@users.noreply.github.com>
2025-06-19 18:05:12 -0300

    ADD: home page, FIX: css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import {Link} from 'react-router-dom';
+import Button from '@mui/material/Button';
+
 import TemporaryDrawer from '../components/TemporaryDrawer';
 
 import '../css/home.css'
@@ -6,7 +9,15 @@ export default function Home() {
     return (
         <div>
             <TemporaryDrawer />
-            <h1>Your expense control app</h1>
+            <div className='main-content'>
+                <h1>Your expense control app</h1>
+                <p>By Sandy Dutra</p>
+            </div>
+            <div className='home-categories'>
+                <Link to="/groceries"><Button>Groceries</Button></Link>
+                <Link to="/clothing"><Button>Clothing</Button></Link>
+                <Link to="/electronics"><Button>Electronics</Button></Link>
+            </div>
         </div>
     );
 }
